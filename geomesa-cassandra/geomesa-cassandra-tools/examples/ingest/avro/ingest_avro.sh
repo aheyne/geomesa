da952c5c6194cb237f1ea5536d20b004e0ec5566
Austin Heyne <aheyne@ccri.com>
2017-06-05 15:07:42 -0400

    Fixing command names
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geomesa-cassandra/geomesa-cassandra-tools/examples/ingest/avro/ingest_avro.sh
+++ geomesa-cassandra/geomesa-cassandra-tools/examples/ingest/avro/ingest_avro.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 # type and converter example-xml is registered in $GEOMESA_HOME/conf/application.conf
-geomesa ingest -u user -p password -i inst -z zoo -c catalog -s example-avro -C example-avro-no-header example_no_header.avro
+bin/geomesa-cassandra ingest \
+    --contact-point 127.0.0.1:9042 \
+    --key-space mykeyspace \
+    --catalog mycatalog \
+    --converter example-avro-no-header \
+    --spec example-avro \
+    examples/ingest/avro/example_no_header.avro
 
-geomesa ingest -u user -p password -i inst -z zoo -c catalog -s example-avro -C example-avro-header example_with_header.avro
+bin/geomesa-cassandra ingest \
+    --contact-point 127.0.0.1:9042 \
+    --key-space mykeyspace \
+    --catalog mycatalog \
+    --converter example-avro-header \
+    --spec example-avro \
+    examples/ingest/avro/example_with_header.avro
